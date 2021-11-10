b3c23bb3e53a5a5192e1470149dd0e6e44cb40f4
Andrew Cummings <acummings@egnyte.com>
2021-11-09 22:27:39 -0800

    some useful util files

---
+++ src/util/useCookie.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+import Cookies from 'universal-cookie';
+
+const useCookie = (key, options = {}) => {
+    const cookies = new Cookies();
+    const [item, setItemValue] = useState(() => {
+        if (cookies.get(key)) {
+            return cookies.get(key);
+        }
+        return null;
+    });
+    
+    const setValue = (value, options) => {
+        setItemValue(value);
+        cookies.set(key, value, options);
+    };
+    
+    const removeItem = (options) => {
+        setItemValue(null);
+        cookies.remove(key);
+    };
+    
+    return [item, setValue, removeItem];
+};
+
+export default useCookie;
