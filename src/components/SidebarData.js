37af995e59c9fe89d0383ba4cb40ba062a44522b
Andrew Cummings <andrewc04@g.ucla.edu>
2021-11-24 19:42:01 -0800

    remove bad menu items

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
 
 const darkIcon = {
   color: "grey"
@@ -9,19 +7,7 @@ const darkIcon = {
 
 export const SidebarData = [
   {
-    title: 'Login',
-    path: '/login',
-    icon: <AiIcons.AiFillHome style={darkIcon}/>,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Sign-Up',
-    path: '/sign-up',
-    icon: <IoIcons.IoIosPaper style={darkIcon}/>,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Find-Friend',
+    title: 'Find Musicians',
     path: '/find-friend',
     icon: <FaIcons.FaCartPlus style={darkIcon}/>,
     cName: 'nav-text'
