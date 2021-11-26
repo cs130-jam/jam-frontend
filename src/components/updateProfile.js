a443261425cca8922c0a861f4fd39a534957b2bb
Sri Keerthi Bolli <srikeerthi207@gmail.com>
2021-11-25 22:43:18 -0800

    jam-form corrected, added update favourite artists

--- src/components/updateProfile.js
+++ src/components/updateProfile.js
@@ -4,7 +4,8 @@ import ErrorInputField from '../util/errorInputField';
 import '../App.css';
 import './updateProfile.css';
 import DropdownField from '../util/dropdownField';
-
+import useInterval from '../util/useInterval';
+import { Alert } from 'react-bootstrap';
 
   const instrumentsListStyle = {
     margin: "0px",
@@ -12,7 +13,43 @@ import DropdownField from '../util/dropdownField';
     listStyleType: "none"
 }
 
+const artistContainer = {
+    height: "60px",
+    position: "relative"
+}
 
+const artistImageStyle = {
+    width: "60px",
+    height: "60px",
+    borderRadius: "3px"
+}
+
+const artistNameStyle = {
+    fontSize: "18pt",
+    position: "absolute",
+    top: "15px",
+    left: "70px"
+}
+
+const artistButtonStyle = {
+    marginTop: "18px",
+    marginRight: "-10px"
+}
+
+const alertStyle = {
+    top: '0'
+};
+
+const formStyle = {
+
+        padding: "100px 30px 30px 30px",
+        border: "0px",
+        background: "#fff"
+  
+};
+
+const ARTIST_SEARCH_DELAY = 1000; // every second
+const MIN_ARTIST_SEARCH_LEN = 2;
 
 const UpdateProfile = (props) => {
 
@@ -30,7 +67,14 @@ const UpdateProfile = (props) => {
     const [knownInstruments, setKnownInstruments] = useState([]);
     const [instQuery, setInstQuery] = useState("");
 
-    function next() {
+    const [cachedSearches, setCachedSearches] = useState({});
+    const [artistQueryResults, setArtistQueryResults] = useState([]);
+    const [artistQuery, setArtistQuery] = useState("");
+    const [artists, setArtists] = useState([]);
+    const [isValidInterests, setIsValidInterests] = useState(true); 
+    const [failMessage, setFailMessage] = useState("");
+
+    function namesDone() {
         if (firstName.length === 0) {
             return;
         }
@@ -77,6 +121,85 @@ const UpdateProfile = (props) => {
         setPageIndex(pageIndex + 1);
     }
 
+    useInterval(() => {
+        let cached = cachedSearches[artistQuery];
+        getArtistsForQuery(artistQuery, cached ? cached.wantedCount : 10, {...cachedSearches});
+    }, ARTIST_SEARCH_DELAY);
+
+
+    async function getArtistsForQuery(query, wantedCount, cachedSearches) {
+        if (query.length <= MIN_ARTIST_SEARCH_LEN) return null;
+
+        let cached = cachedSearches[query];
+
+        if (cached && cached.wantedCount !== wantedCount) {
+            cached.wantedCount = wantedCount;
+            setCachedSearches(cachedSearches);
+        }
+        if (cached && cached.responses.length >= wantedCount) return;
+        if (cached && cached.page === cached.totalPages) return;
+
+        let page = cached ? cached.page + 1 : 1;
+        let response = await apiService.findArtists(query, page);
+        let json = await response.json();
+        if (!cached) {
+            cachedSearches[query] = {
+                "page": page,
+                "totalPages": json.totalPages,
+                "responses": json.responses,
+                "wantedCount": wantedCount
+            };
+            cached = cachedSearches[query];
+        } else {
+            cached.page = page;
+            cached.totalPages = json.totalPages;
+            cached.responses = [...cached.responses, json.responses];
+        }
+
+        if (cached.responses.length < wantedCount) {
+            getArtistsForQuery(query, wantedCount, cachedSearches);
+        } else {
+            setCachedSearches(cachedSearches);
+        }
+    }
+
+    function onArtistSelect(entry) {
+        if (artists.length >= 2) setIsValidInterests(true);
+        console.log(entry);
+        setArtists([...artists, entry.content]);
+    }
+
+    useEffect(() => {
+        let cached = cachedSearches[artistQuery];
+        if (!cached) {
+            setArtistQueryResults([]);
+            return;
+        }
+
+        setArtistQueryResults(cached.responses.slice(0, cached.wantedCount).map(response => {
+            return {
+                "html": (
+                    <div style={artistContainer}>
+                        <img style={artistImageStyle} src={response.thumb}/>
+                        <div style={artistNameStyle}>{response.name}</div>
+                    </div>
+                    ),
+                "content": response
+            };
+        }));
+    }, [artistQuery, cachedSearches]);
+
+    function moreEntries() {
+        let cached = cachedSearches[artistQuery];
+        if (!cached) return;
+
+        getArtistsForQuery(artistQuery, cached.wantedCount + 10, {...cachedSearches});
+    }
+
+    function removeArtist(removeEntry) {
+        setArtists(artists.filter(artist => artist.path !== removeEntry.path));
+    }
+
     async function loadUser(){
         let response = await apiService.getCurrentUser();
         
@@ -86,23 +209,18 @@ const UpdateProfile = (props) => {
         }
         let json = await response.json();
         console.log(json);
-        console.log(json.profile.instruments);
+        console.log(json.profile.musicInterests);
         setFirstName(json.profile.firstName);
         setLastName(json.profile.lastName);
         setKnownInstruments(json.profile.instruments);
-        /*let recuserId = json.userId;
-
-        let userResponse = await apiService.getUser(recuserId);
-        if (!userResponse.ok) {
-          setLoaded(false);
-          return;
-        }
-        let userJson = await userResponse.json();
-        setSelected(userJson);*/
         setLoaded(true);
     }
 
     useEffect(() => loadUser(), []);
+
+    async function updateProfile() {
+        
+    }
 
     const pages = [
         (
@@ -122,14 +240,14 @@ const UpdateProfile = (props) => {
                 </div>
                 <div className="col-4"></div>
                 <div className="col-4">
-                    <button className="jam-submit-button" onClick={next}>Next</button>
+                    <button className="jam-submit-button" onClick={namesDone}>Next</button>
                 </div>  
             </div>
         </div>
         ),
         (
             <div className="container-fluid g-0">
-            <p className="jam-title-text">Interested Instruments</p>
+            <p className="jam-title-text">Favourite Instruments</p>
             <DropdownField 
                 value={instQuery}
                 onInput={setInstQuery}
@@ -167,8 +285,41 @@ const UpdateProfile = (props) => {
     
           
           <div className="d-flex justify-content-center align-items-center">
-          <div className="jam-form">
-          {pages[pageIndex]}
+          <div className="jam-form" style={formStyle}>
+          <div className="container-fluid g-0">
+                {!isValidInterests && <Alert style={alertStyle} variant="danger">Enter at least 3 artists</Alert>}
+                {failMessage.length > 0 && <Alert style={alertStyle} variant="danger">{failMessage}</Alert>}
+                <p className="jam-title-text">Favourite Artists</p>
+                <DropdownField 
+                    value={artistQuery}
+                    onInput={setArtistQuery}
+                    label="Add Favorite Artists:" 
+                    type="text"
+                    hasMore={cachedSearches[artistQuery] && cachedSearches[artistQuery].page < cachedSearches[artistQuery].totalPages}
+                    onMore={moreEntries}
+                    onSelect={onArtistSelect}
+                    entries={artistQueryResults}/>
+                <ul style={instrumentsListStyle}>
+                    {artists.map(artist => (
+                        <li className="removable-list-entry" key={artist.path}>
+                            <div style={artistContainer}>
+                                <img style={artistImageStyle} src={artist.thumb}/>
+                                <div style={artistNameStyle}>{artist.name}</div>
+                                <button style={artistButtonStyle} onClick={e => removeArtist(artist)}>x</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <div className="row g-0">
+                    <div className="col-4">
+                        <button className="jam-submit-button" onClick={prevPage}>Back</button>
+                    </div>
+                    <div className="col-4"></div>
+                    <div className="col-4">
+                        <button className="jam-submit-button" onClick={updateProfile}>Apply Changes</button>
+                    </div>  
+                </div>
+            </div> 
          
             </div>
             </div>
