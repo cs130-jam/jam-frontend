eb0d97a90844ed0a0b9fdc733aba7c435364377a
Edmund Yim <samyim1212@gmail.com>
2021-11-27 16:31:43 -0800

    Minor changes

--- src/components/groupDetails.js
+++ src/components/groupDetails.js
@@ -1,52 +1,51 @@
 import React, {useState, useRef, useEffect} from 'react';
 import '../App.css';
 import InputField from '../util/inputField';
+import { useHistory } from "react-router";
 
 const GroupDetails = (props) => 
 {
     //api service needed to view gc details
     const apiService = props.apiService;
-    const roomID = props.match.params.roomID;
+    const roomId = props.match.params.roomId;
     const currentUser = props.currentUser;
+    const userID = props.match.params.id;
 
     const [groupName, setGroupName] = useState("");
-    const [groupDetails, setGroupDetails] = useState({});
-    const [loaded, setLoaded] = useState(false);
-    const lastGroupDetails = useRef("");
-    const lastGroupName = useRef("");
+    const [groupDetails, setGroupDetails] = useState("[Put Details Here]");
+    const [chatRoomDetails, setChatRoomDetails] = useState({});
+    const [friends, setFriends] = useState([]);
+    const [selectedFriends, setSelectedFriends] = useState([]);
+    const [friendSearch, setFriendSearch] = useState("");
+    const history = useHistory();
 
-    async function fetchGroupDetails()
+    async function fetchChatRoomDetails()
     {
-        let response = await apiService.getChatRoomDetails(roomID);
+        let response = await apiService.getChatRoomDetails(roomId);
         if(!response.ok)
         {
-            /*Return Group not found in big letters*/
-            return (
-                <p>
-                <center>
-                    Error. Group Not Found.
-                </center>
-                </p>
-            );
+            return;
         }
 
         let json = await response.json();
-        setGroupDetails(json);
-        setLoaded(true);
+        setChatRoomDetails(json);
+        setGroupName(json.info.name);
+        setGroupDetails(json.info.topic);
     }
 
-    async function updateGroupName()
-    {
-        let response = await apiService.setChatRoomDetails(roomID, groupName);
-        lastGroupName.current = groupName;
-        return;
-    }
+    useEffect(() => fetchChatRoomDetails(), []);
 
     async function updateGroupDetails()
     {
-        let response = await apiService.setChatRoomDetails(roomID, groupDetails);
-        lastGroupDetails.current = groupDetails;
-        return;
+        let response = await apiService.setChatRoomDetails({
+            "name": groupName,
+            "topic": groupDetails
+        });
+        if (response.ok)
+        {
+            history.push("/chatrooms/" + roomId);
+        }
+        console.log(response);
     }
 
     async function viewProfile()
@@ -56,46 +55,44 @@ const GroupDetails = (props) =>
 
     async function inviteMember()
     {
-        let response = await apiService.inviteMember(roomID, userID);
+        let response = await apiService.inviteMember(roomId, userID);
         return;
     }
 
     async function leaveGroup()
     {
-        let response = apiService.leaveGroup(roomID);
+        let response = apiService.leaveGroup(roomId);
         return;
     }
 
     async function removeMember()
     {
-        let response = apiService.removeMember(roomID, userID);
+        let response = apiService.removeMember(roomId, userID);
         return;
     }
 
-    useEffect(() => fetchGroupDetails(), []);
-
     //Display the page/extra things to view on page
+    const page = (
+        <div className="container-fluid g-0">
+        <p>
+            <InputField value={groupName} onInput={setGroupName} label="Group Name:" type="text" id="groupName"/>
+        </p>
+        <p>
+            <InputField value={groupDetails} onInput={setGroupDetails} label="Group Details:" type="text" id="groupDetail"/>
+            <center><button class="edit-btn edit-btn1" onClick={updateGroupDetails}>Save</button></center>
+        </p>
+        </div>
+    );
+
     return (
-        loaded && 
         <div>
-        <center>
-            <img class="group-chat-pic" src="https://m.media-amazon.com/images/I/51Z7IMEhjKL._AC_SX425_.jpg" alt="Pfp" width="144" height="144"></img>
-            <button class="edit-btn edit-btn1">Change Image</button>
-        </center>
-        <p>
-            <center>
-            Group Name: 
-            <InputField value={groupName} onInput={setGroupName} label="groupName:" type="groupName" />
-            <button class="edit-btn edit-btn1" onClick={updateGroupName}>Save</button>
-            </center>
-        </p>
-        <p>
-            <center>
-            Group Details: 
-            <InputField value={groupDetails} onInput={setGroupDetails} label="groupDetails:" type="groupDetails" />
-            <button class="edit-btn edit-btn1" onClick={updateGroupDetails}>Save</button>
-            </center>
-        </p>
+        <div className="d-flex justify-content-center align-items-center">
+          <div>
+            {page}
+            </div>
+        </div>
+        
+        <div>
         <p>
             <center>
             <button class="edit-btn edit-btn1" onClick={inviteMember}>Invite Members</button>
@@ -106,17 +103,18 @@ const GroupDetails = (props) =>
             <center>
             Group Members:
             <ul>
-                {groupDetails.members.map(member => (
+                {/*groupDetails.members.map(member => (
                     <li className="member-entry" key={member.id}>
                         <span>{member.profile.firstName + " " + member.profile.lastName}</span>
                         <button class="edit-btn edit-btn1" onClick={() => viewProfile(member.id)}>View Profile</button>
                         {(groupDetails.info.admin === currentUser.id) && 
                             <button class="edit-btn edit-btn1" onClick={() => removeMember(member.id)}>Remove Member</button>}
                     </li>
-                ))}
+                        ))*/}
             </ul>
             </center>
         </p>
+        </div>
         </div>
     );
 };
