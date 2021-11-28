9b44a2837337980cc47e355c6931e59a43a17854
ytandon <yashtn@gmail.com>
2021-11-27 22:49:52 -0800

    Add files via upload
    Notifications frontend component progress

--- src/components/notifications.js
+++ src/components/notifications.js
@@ -1,9 +1,46 @@
 import { useEffect, useState } from "react";
+import * as FaIcons from 'react-icons/fa';
+import React from 'react';
+import "./notifications.css";
+
+const listStyle = {
+    margin: "0 auto",
+    width: "800px",
+    listStyle: "none",
+    marginTop: "40px"
+}
+
+const notifStyle = {
+    float: "right",
+    marginTop: "8px"
+}
+
 
 const Notifications = (props) => {
     const apiService = props.apiService;
 
     const [notifications, setNotifications] = useState([]);
+
+
+    async function removeNotification(id) {
+        let response = await apiService.remNotification(id);
+        if (!response.ok) return;
+        console.log("removeNotification");
+        getNotifications();
+    }
+
+    async function rejectNotification(id) {
+        let response = await apiService.rejNotification(id);
+        if (!response.ok) return;
+        console.log("rejectNotification");
+        getNotifications();
+    }
+    async function acceptNotification(id) {
+        let response = await apiService.accNotification(id);
+        if (!response.ok) return;
+        console.log("acceptNotification");
+        getNotifications();
+    }
 
     async function getNotifications() {
         let response = await apiService.getNotifications();
@@ -12,12 +49,19 @@ const Notifications = (props) => {
     }
     useEffect(() => getNotifications(), []);
 
-    return (<>
+    return (<ul style = {listStyle}>
         {notifications.map(notification =>
-            <div>{notification.title}</div> 
+            <li> 
+                <div className="notification-entry">{notification.title} 
+                    <button onClick={acceptNotification(notification.id)}> <FaIcons.FaCheck style={notifStyle}/> </button> 
+                    <button onClick={rejectNotification(notification.id)}> <FaIcons.FaRegTimesCircle style={notifStyle} /> </button> 
+                    <button onClick={removeNotification(notification.id)}> <FaIcons.FaTrashAlt style={notifStyle}/> </button>
+                </div>
+                </li> 
         )}
-    </>
+    </ul>
     );
 };
 
 export default Notifications;
+//FaConciergeBell
