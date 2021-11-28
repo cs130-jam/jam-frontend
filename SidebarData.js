f0366fb29e55e4d36d6fcdea1b9de0bf28db58ff
ytandon <yashtn@gmail.com>
2021-11-27 22:50:56 -0800

    Add files via upload
    Added sidebar logo

---
+++ SidebarData.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+
+const darkIcon = {
+  color: "grey"
+}
+
+export const SidebarData = [
+  {
+    title: 'Update Profile',
+    path: '/update-profile',
+    icon: <FaIcons.FaUserEdit style={darkIcon}/>,
+  },
+  {
+    title: 'Suggestions',
+    path: '/find-friend',
+    icon: <FaIcons.FaHandPaper style={darkIcon}/>,
+  },
+  {
+    title: 'My Friends',
+    path: '/view-friends',
+    icon: <FaIcons.FaUserFriends style={darkIcon}/>,
+  },
+  {
+    title: 'Chatrooms',
+    path: '/chatrooms',
+    icon: <FaIcons.FaCommentAlt style={darkIcon}/>
+  },
+  {
+    title: 'Notifications',
+    path: '/notifications',
+    icon: <FaIcons.FaConciergeBell style={darkIcon}/>
+  },
+  {
+    title: 'Logout',
+    path: '/logout',
+    icon: <FaIcons.FaSignOutAlt style={darkIcon}/>
+  }
+];
