b85e64cb3f4084eab8bcbb15bb95ce8a8e2aae91
Sri Keerthi Bolli <srikeerthi207@gmail.com>
2021-11-28 19:24:47 -0800

    contact us component added

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -1,14 +1,86 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useHistory } from "react-router-dom";
+import '../App.css';
+import {Link} from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert'
+import InputField from '../util/inputField';
 
-import ReactDOM from 'react-dom';
+const linkStyle = {
+    color: 'black'
+};
 
+const linkContainer = {
+    textAlign: "center",
+    width: "100%",
+    display: "inline-block",
+};
 
-const ContactUs = () => {
-  return (
-    <div >
-        <h1>Contact us</h1> 
-    </div>
-  );
+const alertStyle = {
+    top: '0'
+};
+
+const msgStyle = {
+ width: "400px",
+ height: "200px"
+};
+
+const ContactUs = (props) => {
+    
+    
+    const history = useHistory();
+
+    const [username, setUsername] = useState("");
+    const [mail, setMail] = useState("");
+    const [message, setMessage] = useState("");
+
+    async function handleLogin(loginData) {
+        
+            history.push("/login");
+       
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        handleLogin({
+          username: username,
+          mail: mail,
+          message: message
+        });
+    };
+
+    function onUsername(name) {
+      setUsername(name);
+    }
+
+    function onMail(mail) {
+        setMail(mail);
+
+    }
+
+    function onMessage(message) {
+      setMessage(message);
+
+  }
+
+    return (
+        <div className="d-flex justify-content-center align-items-center">
+            <form className="jam-form" onSubmit={handleSubmit}>
+          
+                <p className="jam-title-text">Contact Us</p>
+                <InputField label="Enter your Email ID: " type="text" value={mail} onInput={onMail}/>
+                <InputField label="Enter your Name: " type="text" value={username} onInput={onUsername}/>
+                <br/>
+                <textarea style={msgStyle}>
+                    Enter your message here
+                </textarea>
+                
+                <div>
+                    <button className="jam-submit-button" type="submit">Submit</button>
+                </div>
+
+            </form>
+        </div>
+    );
 };
 
 export default ContactUs;
